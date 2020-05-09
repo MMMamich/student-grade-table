1f61e4aeca66661aa16f81355f8a6fa35fd38e57
mmmamich <mmmamich@gmail.com>
2020-05-09 18:15:57 +0000

    forgot these changes

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
-const TABLE       = document.querySelector('table');
-const GRADE_TABLE = new GradeTable(TABLE);
+class App {
+    constructor(gradeTable, pageHeader){
+      this.gradeTable = gradeTable;
+      this.pageHeader = pageHeader;
 
-const GRADES = new App(GRADE_TABLE);
-GRADES.start();
+      this.handleGetGradesError   = this.handleGetGradesError.bind(this);
+      this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+    }
+
+    handleGetGradesError(err){
+      console.error(err);
+    }
+    handleGetGradesSuccess(grades){
+      this.gradeTable.updateGrades(grades);
+      this.pageHeader.updateAverage(Math.floor((grades[0].grade) + (grades[1].grade) + (grades[2].grade)) / 3)
+    }
+    getGrades(){
+        $.ajax({
+           method: "GET",
+           url: 'https://sgt.lfzprototypes.com/api/grades',
+           headers: { "X-Access-Token": "EIX4Pjaf"},
+           success: grades => this.handleGetGradesSuccess(grades),
+           error: error => this.handleGetGradesError(error)
+        })
+    }
+    start(){
+        this.getGrades();
+    }
+}
