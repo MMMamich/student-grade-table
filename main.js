72554a44c1408cb4df4e9046aee27d096bf9eb9e
mmmamich <mmmamich@gmail.com>
2020-05-09 21:06:10 +0000

    added all configuration steps

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+const FORM        = document.querySelector('form');
+const PAGE_FORM   = new GradeForm(FORM);
+
 const HEADER      = document.querySelector('.grade');
 const PAGE_HEADER = new PageHeader(HEADER);
 
 const TABLE       = document.querySelector('table');
 const GRADE_TABLE = new GradeTable(TABLE);
 
-const GRADES = new App(GRADE_TABLE, PAGE_HEADER);
+const GRADES = new App(GRADE_TABLE, PAGE_HEADER, PAGE_FORM);
 GRADES.start();
